71c621ceafb891aa439995dcbe9055179680dcb4
Anuj das <anujd339@gmail.com>
2025-01-21 15:48:38 +0530

    Update LastAssetCode.js

--- backend/models/LastAssetCode.js
+++ backend/models/LastAssetCode.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+// Last Asset Code Schema
+const lastAssetCodeSchema = new mongoose.Schema({
+  lastCode: { type: Number, required: true },
+});
+
+const LastAssetCode = mongoose.model('LastAssetCode', lastAssetCodeSchema);
+module.exports = LastAssetCode;
 
