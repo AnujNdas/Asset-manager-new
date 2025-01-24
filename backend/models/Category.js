5ac5d14428beea77e7bb5081e7f3a8ce7f00f91d
Anuj das <anujd339@gmail.com>
2025-01-25 00:20:05 +0530

    Update Category.js

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -1,1 +1,16 @@
+// models/category.js
+const mongoose = require('mongoose');
+
+// Define the schema for Category
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true, // Optional, if you want unique categories
+  },
+}, { timestamps: true });
+
+const Category = mongoose.model('Category', categorySchema);
+
+module.exports = Category;
 
