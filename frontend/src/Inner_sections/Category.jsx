a5a6b3decc9892739c115e3ba923bae417d93d61
Anuj das <anujd339@gmail.com>
2025-01-24 23:11:37 +0530

    Update Category.jsx

--- frontend/src/Inner_sections/Category.jsx
+++ frontend/src/Inner_sections/Category.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
+
+const Category = () => {
+  return (
+    <div className='classify_content_box'>
+        <div className="input">
+            <p> Enter Category :- </p>
+            <input type="text" />
+        </div>
+        <div className="input_content">
+
+        </div>
+    </div>
+  )
+}
+
+export default Category
 
