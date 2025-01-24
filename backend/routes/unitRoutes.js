ba41848e85a3efa9f143e9c79e698256a68c7672
Anuj das <anujd339@gmail.com>
2025-01-25 00:22:42 +0530

    Update unitRoutes.js

--- backend/routes/unitRoutes.js
+++ backend/routes/unitRoutes.js
@@ -1,1 +1,13 @@
+// routes/unitRoutes.js
+const express = require('express');
+const unitController = require('../controllers/unitControllers');
+const router = express.Router();
+
+// Route to create a unit
+router.post('/', unitController.createUnit);
+
+// Route to get all units
+router.get('/', unitController.getUnits);
+
+module.exports = router;
 
