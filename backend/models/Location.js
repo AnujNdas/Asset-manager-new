3d2b0d2553725bcf62e6321af778fd67f9491281
Anuj das <anujd339@gmail.com>
2025-01-25 00:20:26 +0530

    Update Location.js

--- backend/models/Location.js
+++ backend/models/Location.js
@@ -1,1 +1,17 @@
+// models/location.js
+const mongoose = require('mongoose');
 
+// Define the schema for Location
+const locationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true, // Optional, if you want unique locations
+  },
+}, { timestamps: true });
+
+const Location = mongoose.model('Location', locationSchema);
+
+module.exports = Location;
+
+  
