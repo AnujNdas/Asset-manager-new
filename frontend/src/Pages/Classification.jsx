f8ffedaba36a973fbebdb91cc3e1de86d03a16f5
Anuj das <anujd339@gmail.com>
2025-01-24 23:10:55 +0530

    Update Classification.jsx

--- frontend/src/Pages/Classification.jsx
+++ frontend/src/Pages/Classification.jsx
@@ -1,1 +1,37 @@
+import React from 'react'
+import { Link, Route, Routes } from 'react-router-dom'
+import '../Page_styles/Classification.css'
+import Unit from '../Inner_sections/Unit'
+import Category from '../Inner_sections/Category'
+import Location from '../Inner_sections/Location'
+const Classification = () => {
+  return (
+    <div className='classification_container'>
+        <div className="classify_heading">
+            Classification
+        </div>
+        <div className="classification_inner">
+            <div className="classify_menu">
+                <div className="menu_div">
+                    <Link to="/classification/location">Location</Link>
+                    <Link to="/classification/unit">Unit</Link>
+                    <Link to="/classification/category">Category</Link>
+                    <Link to="/category">Category</Link>
+                </div>
+            </div>
+            <div className="classify_items">
+                <div className="classify_content">
+                    <Routes>
+                        <Route exact path='/unit' element={<Unit/>} />
+                        <Route exact path='/category' element={<Category/>} />
+                        <Route exact path='/location' element={<Location/>} />
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Classification
 
