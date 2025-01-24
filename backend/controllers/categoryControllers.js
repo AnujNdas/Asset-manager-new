36080ec76821ec777460aa6d9e64d3060bbecac8
Anuj das <anujd339@gmail.com>
2025-01-25 00:17:50 +0530

    Update categoryControllers.js

--- backend/controllers/categoryControllers.js
+++ backend/controllers/categoryControllers.js
@@ -1,1 +1,27 @@
+// controllers/categoryController.js
+const Category = require('../models/Category');
+
+// Create a new category
+exports.createCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const newCategory = new Category({ name });
+    await newCategory.save();
+    res.status(201).json(newCategory);
+  } catch (error) {
+    console.error('Error creating category:', error);
+    res.status(500).json({ error: 'Error creating category' });
+  }
+};
+
+// Get all categories
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    res.status(500).json({ error: 'Error fetching categories' });
+  }
+};
 
