ecc9c9f34e6c1a37332e26af6392902eecb95c9e
Anuj das <anujd339@gmail.com>
2025-01-25 00:22:27 +0530

    Update locationRoutes.js

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,1 +1,13 @@
+// routes/locationRoutes.js
+const express = require('express');
+const locationController = require('../controllers/locationControllers');
+const router = express.Router();
+
+// Route to create a location
+router.post('/', locationController.createLocation);
+
+// Route to get all locations
+router.get('/', locationController.getLocations);
+
+module.exports = router;
 
