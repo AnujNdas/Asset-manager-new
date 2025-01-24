030e04b1b9c071a2d7f2ee3bee263dbd4bc00685
Anuj das <anujd339@gmail.com>
2025-01-24 23:12:09 +0530

    Update Location.jsx

--- frontend/src/Inner_sections/Location.jsx
+++ frontend/src/Inner_sections/Location.jsx
@@ -1,1 +1,18 @@
+import React from 'react'
+
+const Location = () => {
+  return (
+    <div className='classify_content_box'>
+        <div className="input">
+            <p> Enter Location :- </p>
+            <input type="text" />
+        </div>
+        <div className="input_content">
+
+        </div>
+    </div>
+  )
+}
+
+export default Location
 
