62f2c28958a622fc96f3c099b1f29806e16f30dc
Anuj das <anujd339@gmail.com>
2025-01-24 23:12:46 +0530

    Update Unit.jsx

--- frontend/src/Inner_sections/Unit.jsx
+++ frontend/src/Inner_sections/Unit.jsx
@@ -1,1 +1,17 @@
+import React from 'react'
+import '../Page_styles/Unit.css'
+const Unit = () => {
+  return (
+    <div className='classify_content_box'>
+        <div className="input">
+            <p> Enter Unit :- </p>
+            <input type="text" />
+        </div>
+        <div className="input_content">
 
+        </div>
+    </div>
+  )
+}
+
+export default Unit
