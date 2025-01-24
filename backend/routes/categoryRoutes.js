1fd9134b50afd96982f84de316fde0a72a6e3350
Anuj das <anujd339@gmail.com>
2025-01-25 00:22:11 +0530

    Update categoryRoutes.js

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,1 +1,13 @@
+// routes/categoryRoutes.js
+const express = require('express');
+const categoryController = require('../controllers/categoryControllers');
+const router = express.Router();
+
+// Route to create a category
+router.post('/', categoryController.createCategory);
+
+// Route to get all categories
+router.get('/', categoryController.getCategories);
+
+module.exports = router;
 
