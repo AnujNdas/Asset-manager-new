19e859237ac79da2c29699b995cfa15a01a3e7e5
Anuj das <anujd339@gmail.com>
2025-01-25 00:20:43 +0530

    Update Unit.js

--- backend/models/Unit.js
+++ backend/models/Unit.js
@@ -1,1 +1,16 @@
+// models/unit.js
+const mongoose = require('mongoose');
+
+// Define the schema for Unit
+const unitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true, // Optional, if you want unique units
+  },
+}, { timestamps: true });  // Optional, adds createdAt and updatedAt fields
+
+const Unit = mongoose.model('Unit', unitSchema);
+
+module.exports = Unit;
 
