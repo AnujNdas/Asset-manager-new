410e6ef3ecff50cf4efbc1a0be48b6d41e682a42
Anuj das <anujd339@gmail.com>
2025-01-25 15:36:04 +0530

    Update statusRoutes.js

--- backend/routes/statusRoutes.js
+++ backend/routes/statusRoutes.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const { createStatus, getStatuses } = require('../controllers/statusControllers');
 
+const router = express.Router();
+
+// Route for creating a new status
+router.post('/', createStatus);
+
+// Route for getting all statuses
+router.get('/', getStatuses);
+
+module.exports = router;
