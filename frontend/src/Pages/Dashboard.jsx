6c643b50f6f1d4b1f2c0f7c8f4dba786aa5965ec
Anuj das <anujd339@gmail.com>
2025-02-12 15:27:29 +0530

    Update Dashboard.jsx

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -1,18 +1,85 @@
 import React from 'react'
 import '../Page_styles/Dashboard.css'
-import Sectionone from '../Inner_sections/Sectionone'
-import Sectiontwo from '../Inner_sections/Sectiontwo'
-import AssetDashboard from '../Inner_sections/AssetDashboard'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLocationDot,faList,faChartSimple,faEnvelopesBulk} from '@fortawesome/free-solid-svg-icons';
+
 const Dashboard = () => {
   return (
     <div className='dashboard-section'>
-      <AssetDashboard/>
-      {/* <div className='section_1'>
-        <Sectionone/>
+      <div className="classify_heading">
+        Assets Overview
       </div>
-      <div className='section_2'>
-        <Sectiontwo/>
-      </div> */}
+      <div className='card-section'>
+        <div className="card" >
+          <div className="card-info" style={{
+          backgroundColor : "#3f4045"
+        }}>
+          <FontAwesomeIcon icon={faChartSimple} style={{
+            color : "white",
+            fontSize : "20px",
+            border: "none",
+            textAlign: "center",
+            textShadow: "2px 2px 8px rgba(0, 0, 0, 0.6)"
+          }}/>
+          </div>
+        </div>
+        <div className="card" >
+          <div className="card-info" style={{
+          backgroundColor : "#3d97ef"
+        }}>
+          <FontAwesomeIcon icon={faEnvelopesBulk} style={{
+            color : "white",
+            fontSize : "20px",
+            border: "none",
+            textAlign: "center",
+            textShadow : "0px 0px 5px 15px rgba(0,0,0,0.5)"
+          }}/>
+          </div>
+        </div>
+        <div className="card" >
+          <div className="card-info" style={{
+          backgroundColor : "#5cb762"
+        }}>
+          <FontAwesomeIcon icon={faList} style={{
+            color : "white",
+            fontSize : "20px",
+            border: "none",
+            textAlign: "center",
+            textShadow : "0px 0px 5px 15px rgba(0,0,0,0.5)"
+          }}/>
+          </div>
+        </div>
+        <div className="card" >
+          <div className="card-info" style={{
+          backgroundColor : "orange"
+        }}>
+          <FontAwesomeIcon icon={faLocationDot} style={{
+            color : "white",
+            fontSize : "20px",
+            border: "none",
+            textAlign: "center",
+            textShadow : "0px 0px 5px 15px rgba(0,0,0,0.5)"
+          }}/>
+          </div>
+        </div>
+      </div>
+      <div className="graph-section">
+        <div className="graph">
+          <div className="graph-info" style={{
+            backgroundColor : "#3f4045"
+          }}></div>
+        </div>
+        <div className="graph">
+          <div className="graph-info" style={{
+            backgroundColor : "#3d97ef"
+          }}></div>
+        </div>
+        <div className="graph">
+          <div className="graph-info" style={{
+            backgroundColor : "#5cb762"
+          }}></div>
+        </div>
+      </div>
     </div>
   )
 }
