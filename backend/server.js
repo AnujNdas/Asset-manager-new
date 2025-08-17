6acf3266b3c25dbd1fe801bd76178d993bc6a9f6
Anuj das <anujd339@gmail.com>
2025-08-17 12:50:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,6 @@ app.get("/",(req,res)=>{
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, () =>
     console.log(`Server is running on PORT : ${PORT}`));
