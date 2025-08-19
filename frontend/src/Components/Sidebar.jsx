85aa20951496e57f23a8a4f1c8b42a3887cee8a0
Anuj das <anujd339@gmail.com>
2025-08-19 15:48:14 +0530

    Update Sidebar.jsx

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -22,13 +22,13 @@ const Sidebar = ({closeSidebar})=> {
         </div>
         <div className='sidebar-menu'>
           <ul>
-            <li><Link to="/" onClick={closeSidebar}><FontAwesomeIcon icon={faGauge} style={{ width : "25%"}}/>Dashboard</Link></li>
-            <li><Link to="/AssetCapture" onClick={closeSidebar}><FontAwesomeIcon icon={faCartShopping} style={{ width : "25%"}}/>Asset Capture</Link></li>
+            <li><Link to="/" onClick={closeSidebar}><FontAwesomeIcon icon={faGauge} style={{ width : "25%"}}/><span className="tab-text">Dashboard</span></Link></li>
+            <li><Link to="/AssetCapture" onClick={closeSidebar}><FontAwesomeIcon icon={faCartShopping} style={{ width : "25%"}}/><span className="tab-text">Asset capture</span></Link></li>
             {/* <li><Link to="/Product_list"><FontAwesomeIcon icon={faList} />Product List</Link></li> */}
-            <li><Link to="/Inventory" onClick={closeSidebar}><FontAwesomeIcon icon={faLayerGroup} style={{ width : "25%"}}/>Inventory</Link></li>
-            <li><Link to="/MISReport" onClick={closeSidebar}><FontAwesomeIcon icon={faReceipt} style={{ width : "25%"}}/>MIS Report</Link></li>
-            <li><Link to="/Setting" onClick={closeSidebar}><FontAwesomeIcon icon={faGear} style={{ width : "25%"}}/>Settings</Link></li>
-            <li><Link to="/Classification" onClick={closeSidebar}><FontAwesomeIcon icon={faRecycle} style={{ width : "25%"}}/>Classification</Link></li>
+            <li><Link to="/Inventory" onClick={closeSidebar}><FontAwesomeIcon icon={faLayerGroup} style={{ width : "25%"}}/><span className="tab-text">Inventory</span></Link></li>
+            <li><Link to="/MISReport" onClick={closeSidebar}><FontAwesomeIcon icon={faReceipt} style={{ width : "25%"}}/><span className="tab-text">MIS Report</span></Link></li>
+            <li><Link to="/Setting" onClick={closeSidebar}><FontAwesomeIcon icon={faGear} style={{ width : "25%"}}/><span className="tab-text">Settings</span></Link></li>
+            <li><Link to="/Classification" onClick={closeSidebar}><FontAwesomeIcon icon={faRecycle} style={{ width : "25%"}}/><span className="tab-text">Classifications</span></Link></li>
           </ul>
         </div>
       </div>
