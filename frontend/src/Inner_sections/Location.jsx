6c33dec94c19fd7bbda0c1ecb157c4946c28571e
Anuj das <anujd339@gmail.com>
2025-08-19 12:01:29 +0530

    Update Location.jsx

--- frontend/src/Inner_sections/Location.jsx
+++ frontend/src/Inner_sections/Location.jsx
@@ -106,7 +106,7 @@ const Location = () => {
     disabled={classifyCurrentPage === 1}
     className={`pagination ${classifyCurrentPage === 1 ? 'disabled' : ''}`}
   >
-    Previous
+    Prev
   </button>
 
   {Array.from({ length: totalClassifyPages }, (_, index) => (
