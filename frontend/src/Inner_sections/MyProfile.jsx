167a6ce1bee0ccaa251899456ef086b46688511f
Anuj das <anujd339@gmail.com>
2025-08-21 17:10:18 +0530

    Update MyProfile.jsx

--- frontend/src/Inner_sections/MyProfile.jsx
+++ frontend/src/Inner_sections/MyProfile.jsx
@@ -19,7 +19,7 @@ const MyProfile = () => {
       }
 
       try {
-        const response = await fetch("http://localhost:5000/api/auth/user", {
+        const response = await fetch("https://asset-manager-new.onrender.com/api/auth/user", {
           method: "GET",
           headers: {
             "Authorization": `Bearer ${token}`, // Send token in the Authorization header
