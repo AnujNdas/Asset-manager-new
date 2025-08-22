6ea10c3a6f394c84ca3f4cb3a2ef1f2c69a0bf04
Anuj das <anujd339@gmail.com>
2025-08-22 14:32:04 +0530

    Update EditButton.jsx

--- frontend/src/Components/EditButton.jsx
+++ frontend/src/Components/EditButton.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faPenToSquare} from '@fortawesome/free-solid-svg-icons';
-const EditButton = () => {
-  return (
-    <div className='edit-btn' style={{height : "35px",textAlign: "center" , width : '70px' , borderRadius : '20px', backgroundColor : "#f6f6f6", fontSize: "14px", display:"flex" , alignItems : "center" , justifyContent:"center", gap: '5px'}}>
-      Edit <FontAwesomeIcon icon={faPenToSquare} />
-    </div>
-  )
-}
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 
-export default EditButton
+const EditButton = ({
+  text = "Edit",
+  icon = faPenToSquare,
+  onClick,
+  backgroundColor = "#f6f6f6",
+  color = "#333",
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      style={{
+        height: "35px",
+        width: "80px",
+        borderRadius: "20px",
+        backgroundColor,
+        color,
+        fontSize: "14px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "5px",
+        border: "none",
+        cursor: "pointer",
+        transition: "background 0.3s ease",
+      }}
+      onMouseOver={(e) => (e.currentTarget.style.backgroundColor = "#eaeaea")}
+      onMouseOut={(e) => (e.currentTarget.style.backgroundColor = backgroundColor)}
+    >
+      {text} <FontAwesomeIcon icon={icon} />
+    </button>
+  );
+};
+
+export default EditButton;
