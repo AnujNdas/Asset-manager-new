6ea1f9d191fe2ac1e193e08f40f9c64680de0b9a
Anuj das <anujd339@gmail.com>
2025-08-22 14:18:51 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,28 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type : String,
-        required : true,
-    },
-    username : {
-        type : String ,
-        required: true,
-        unique : true,
-    },
-    password : {
-        type : String , 
-        required : true,
-        unique : true,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  bio: { type: String, default: "" },
+  email: { type: String, default: "" },
+  phone: { type: String, default: "" },
+  country: { type: String, default: "" },
+  city: { type: String, default: "" },
+  postalCode: { type: String, default: "" },
+  taxId: { type: String, default: "" },
 });
 
-const User = mongoose.model("user",UserSchema);
-module.exports = User
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
