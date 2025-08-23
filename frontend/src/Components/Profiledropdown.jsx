d68757ce60fe1a505898aebdc5c8d816d784a85c
Anuj das <anujd339@gmail.com>
2025-08-23 15:59:02 +0530

    Update Profiledropdown.jsx

--- frontend/src/Components/Profiledropdown.jsx
+++ frontend/src/Components/Profiledropdown.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faInbox, faMoon } from '@fortawesome/free-solid-svg-icons';
 import '../Page_styles/Profiledropdown.css';
 import { Link } from 'react-router-dom';
 
-const ProfileDropdown = ({ isVisible }) => {
+const ProfileDropdown = ({ isVisible, onClose ,toggleButtonRef }) => {
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      const clickedOutsideDropdown =
+        dropdownRef.current && !dropdownRef.current.contains(event.target);
+      const clickedOutsideToggleButton =
+        toggleButtonRef.current && !toggleButtonRef.current.contains(event.target);
+
+      if (clickedOutsideDropdown && clickedOutsideToggleButton) {
+        onClose();
+      }
+    };
+
+    if (isVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isVisible, onClose, toggleButtonRef]);
+
   return (
-    <div className={`profile-dropdown-container ${isVisible ? 'show' : ''}`}>
+    <div ref={dropdownRef} className={`profile-dropdown-container ${isVisible ? 'show' : ''}`}>
       <div className={`dropdown-menu ${isVisible ? 'show' : ''}`}>
-        <ul>
+        <ul className='drop-box'>
           <li><Link to="/User/Login"><FontAwesomeIcon icon={faUser} /> User</Link></li>
           <li><Link to="/Setting/Notification"><FontAwesomeIcon icon={faInbox} /> Inbox</Link></li>
           <li><Link to="/mode"><FontAwesomeIcon icon={faMoon} /> Mode</Link></li>
@@ -19,4 +44,7 @@ const ProfileDropdown = ({ isVisible }) => {
 };
 
 export default ProfileDropdown;
+
+
+
 
