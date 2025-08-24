b161e346a1a202ef8cc3a57c73b206336de10974
Anuj das <anujd339@gmail.com>
2025-08-24 13:51:39 +0530

    Update AuthService.js

--- frontend/src/Services/AuthService.js
+++ frontend/src/Services/AuthService.js
@@ -4,8 +4,8 @@ const API_URL = "https://asset-manager-new.onrender.com/api/auth";
 // Base url for backend API
 
 //Sign up function
-const signup = async (email ,username , password) => {
-    const response = await axios.post(`${API_URL}/signup`,{  email ,username , password });
+const signup = async (username , email, password) => {
+    const response = await axios.post(`${API_URL}/signup`,{  username , email , password });
     return response.data;
 };
 
