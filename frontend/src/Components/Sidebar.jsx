452dec6dcb50ddd66f043e8925b83cb2d2080f08
Anuj das <anujd339@gmail.com>
2025-08-29 14:04:07 +0530

    Update Sidebar.jsx

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React ,{ useContext} from 'react'
 import '../Component_styles/Sidebar.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from 'react-router-dom'
 import image from "../Images/logo.png"
-import { faGauge ,faLayerGroup ,faCartShopping , faWarehouse, faGear, faReceipt, faRecycle} from '@fortawesome/free-solid-svg-icons'
+import { ThemeContext } from '../Context/ThemeContext'
+import { faGauge ,faLayerGroup ,faCartShopping , faMoon ,faSun, faGear, faReceipt, faRecycle} from '@fortawesome/free-solid-svg-icons'
+// import { useContext } from 'react'
 
 const Sidebar = ({closeSidebar})=> {
+    const { theme, toggleTheme } = useContext(ThemeContext);
   return (
-    <div className='sidebar-container'>
-      <div className='sidebar'>
+    <div className={`sidebar-container ${theme}`}>
+      <div className={`sidebar ${theme}`}>
         <div className='sidebar-heading'>
           <div className="logo">
             <img src={image} style={{
@@ -17,7 +20,7 @@ const Sidebar = ({closeSidebar})=> {
             }}/>
           </div>
           <div className="title">
-            <h1>Asset Manager</h1>
+            <h1>ASSERA</h1>
           </div>
         </div>
         <div className='sidebar-menu'>
@@ -26,10 +29,17 @@ const Sidebar = ({closeSidebar})=> {
             <li><Link to="/assetCapture" onClick={closeSidebar}><FontAwesomeIcon icon={faCartShopping} style={{ width : "25%"}}/><span className="tab-text">Asset capture</span></Link></li>
             {/* <li><Link to="/Product_list"><FontAwesomeIcon icon={faList} />Product List</Link></li> */}
             <li><Link to="/inventory" onClick={closeSidebar}><FontAwesomeIcon icon={faLayerGroup} style={{ width : "25%"}}/><span className="tab-text">Inventory</span></Link></li>
-            <li><Link to="/misreport" onClick={closeSidebar}><FontAwesomeIcon icon={faReceipt} style={{ width : "25%"}}/><span className="tab-text">MIS Report</span></Link></li>
+            <li><Link to="/misReport" onClick={closeSidebar}><FontAwesomeIcon icon={faReceipt} style={{ width : "25%"}}/><span className="tab-text">MIS Report</span></Link></li>
             <li><Link to="/setting" onClick={closeSidebar}><FontAwesomeIcon icon={faGear} style={{ width : "25%"}}/><span className="tab-text">Settings</span></Link></li>
             <li><Link to="/classification" onClick={closeSidebar}><FontAwesomeIcon icon={faRecycle} style={{ width : "25%"}}/><span className="tab-text">Classifications</span></Link></li>
           </ul>
+        </div>
+        {/* ✅ Dark/Light Toggle at Bottom */}
+        <div className="sidebar-toggle">
+          <button onClick={toggleTheme}>
+            <FontAwesomeIcon icon={theme === "light" ? faMoon : faSun} /> 
+            <span>{theme === "light" ? "Dark Mode" : "Light Mode"}</span>
+          </button>
         </div>
       </div>
     </div>
