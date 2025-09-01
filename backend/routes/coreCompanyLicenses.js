880f5f99d6bd07ba926dcab8cc55dde3e05437e2
Anuj das <anujd339@gmail.com>
2025-09-01 17:25:32 +0530

    Update coreCompanyLicenses.js

--- backend/routes/coreCompanyLicenses.js
+++ backend/routes/coreCompanyLicenses.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const {
+  createCompanyLicense,
+  getCompanyLicenses,
+  getCompanyLicenseById,
+  updateCompanyLicense,
+  deleteCompanyLicense
+} = require("../controllers/coreCompanyLicenseController");
+
+const router = express.Router();
+
+router.post("/", createCompanyLicense);
+router.get("/", getCompanyLicenses);
+router.get("/:id", getCompanyLicenseById);
+router.put("/:id", updateCompanyLicense);
+router.delete("/:id", deleteCompanyLicense);
+
+module.exports = router;
 
