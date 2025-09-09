474d23e8da5ddfa4f5b3f5802b0a9509fa9d05c8
Anuj das <anujd339@gmail.com>
2025-09-09 13:59:26 +0530

    Update ProtectedRoute.jsx

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -1,15 +1,20 @@
+// src/components/ProtectedRoute.jsx
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
+const ProtectedRoute = ({ children, allowedRoles }) => {
   const token = sessionStorage.getItem("token");
+  const role = sessionStorage.getItem("role"); // read role from storage
 
   if (!token) {
-    // No token → redirect to login
-    return <Navigate to="/user/login" replace/>;
+    return <Navigate to="/user/login" replace />;
   }
 
-  return children; // Token exists → allow access
+  if (allowedRoles && !allowedRoles.includes(role)) {
+    return <Navigate to="/unauthorized" replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
