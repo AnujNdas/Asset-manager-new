b85b48328f91a52513be8bbe780c016809e33dbf
Anuj das <anujd339@gmail.com>
2025-09-11 14:41:45 +0530

    Update authControllers.js

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -140,7 +140,7 @@ const login = async (req, res) => {
       });
     }
     
-    res.json({ message: "Logged in!", token  , role : user.role});
+    res.json({ message: "Logged in!", token  , role : user.role , userId: user._id});
   } catch (error) {
     res.status(500).json({ error: "Error logging in!" });
   }
