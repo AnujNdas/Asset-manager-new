8b92f86f5c3efaa02520c62e4fd545b14335ca0e
Anuj das <anujd339@gmail.com>
2025-09-16 14:57:26 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp-signup", verifyOtpAndSignup);
 router.post("/login", login);
-router.get("/user", authenticateToken(["admin", "user"]), getUserData);
+router.get("/user", authenticateToken(["admin", "user" , "super-admin"]), getUserData);
 router.put("/change-password", authenticateToken(["admin" , "user"]), changePassword);
 
 module.exports = router;
