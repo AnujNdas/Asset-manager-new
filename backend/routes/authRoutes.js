8c7af6cb4e1e1d922bb445a89f3b5c8ccdf82115
Anuj das <anujd339@gmail.com>
2025-09-19 13:12:18 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { sendOtp, verifyOtpAndSignup, login, getUserData, changePassword } = require("../controllers/authControllers");
+const { sendOtp, verifyOtpAndSignup, login, getUserData, changePassword ,forgotPassword , resetPassword } = require("../controllers/authControllers");
 const authenticateToken = require("../Middleware/Authentication-token");
 
 const router = express.Router();
@@ -7,7 +7,10 @@ const router = express.Router();
 router.post("/send-otp", sendOtp);
 router.post("/verify-otp-signup", verifyOtpAndSignup);
 router.post("/login", login);
-router.get("/user", authenticateToken(["admin", "user" , "super-admin"]), getUserData);
-router.put("/change-password", authenticateToken(["admin" , "user"]), changePassword);
+router.get("/user", authenticateToken(), getUserData);
+router.put("/change-password", authenticateToken(), changePassword);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:token", resetPassword);
 
 module.exports = router;
+
