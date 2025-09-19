972f1ae127097e19f5a824b3edf7f3d151cb0ac9
Anuj das <anujd339@gmail.com>
2025-09-19 13:10:43 +0530

    Update authControllers.js

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -4,6 +4,7 @@ const nodemailer = require("nodemailer");
 const Otp = require("../models/Otp");
 const User = require("../models/User");
 const Notification = require("../models/Notification");
+const crypto = require("crypto");
 
 // In-memory OTP store (better: Redis or DB)
 const otpStore = {};
@@ -199,4 +200,67 @@ const changePassword = async (req, res) => {
   }
 };
 
+const forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(404).json({ message: "User not found" });
+
+    const token = crypto.randomBytes(32).toString("hex");
+    user.resetToken = token;
+    user.resetTokenExpiry = Date.now() + 3600000; // valid 1 hour
+    await user.save();
+
+    const resetLink = `https://asset-manager-new-frontend.onrender.com/user/reset/${token}`;
+
+    // Transporter (use your SMTP or Gmail)
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    await transporter.sendMail({
+      from: `"Asset Manager" <${process.env.EMAIL_USER}>`,
+      to: user.email,
+      subject: "Password Reset Request",
+      html: `<p>Click the link below to reset your password:</p>
+             <a href="${resetLink}">${resetLink}</a>`,
+    });
+
+    res.json({ message: "Password reset link sent to your email" });
+  } catch (err) {
+    console.error("Forgot password error:", err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+
+const resetPassword = async (req, res) => {
+  const { token } = req.params;
+  const { password } = req.body;
+
+  try {
+    const user = await User.findOne({
+      resetToken: token,
+      resetTokenExpiry: { $gt: Date.now() }, // valid token
+    });
+
+    if (!user) return res.status(400).json({ message: "Invalid or expired token" });
+
+    user.password = await bcrypt.hash(password, 10);
+    user.resetToken = undefined;
+    user.resetTokenExpiry = undefined;
+
+    await user.save();
+
+    res.json({ message: "Password has been reset successfully" });
+  } catch (err) {
+    console.error("Reset password error:", err);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
 module.exports = { sendOtp, verifyOtpAndSignup, login, getUserData, changePassword };
