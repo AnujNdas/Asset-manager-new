4598d763a093424fb0f548c8e3ca7ee56503f23d
Anuj das <anujd339@gmail.com>
2025-09-20 13:53:51 +0530

    Create axiosInstances.jsx

---
+++ frontend/src/Services/axiosInstances.jsx
@@ -0,0 +1,22 @@
+// src/Services/axiosInstance.js
+import axios from "axios";
+
+const API_URL = "https://asset-manager-new-frontend.onrender.com/api";
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+});
+
+// Automatically attach token if it exists
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = sessionStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default axiosInstance;
