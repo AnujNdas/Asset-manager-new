2939845204a12f79113f9b0cfb7436716d00ae40
Anuj das <anujd339@gmail.com>
2025-09-20 13:56:40 +0530

    Update Notification.jsx

--- frontend/src/Inner_sections/Notification.jsx
+++ frontend/src/Inner_sections/Notification.jsx
@@ -1,54 +1,9 @@
-import { useState, useEffect , useRef } from "react";
 import { FaBell, FaCheck } from "react-icons/fa";
-import axios from "axios";
-import { io } from "socket.io-client";
-import "../Page_styles/Notification.css"
+import { useNotifications } from "../Context/NotificationContext";
+import "../Page_styles/Notification.css";
 
 const NotificationPage = () => {
-  const [notifications, setNotifications] = useState([]);
-
-  const token = sessionStorage.getItem("token");
-  const userId = sessionStorage.getItem("userId"); // ✅ Store this during login
-
-  const socketRef = useRef()
-
-useEffect(() => {
-  if (!userId) return;
-
-  socketRef.current = io("https://asset-manager-new.onrender.com");
-  socketRef.current.on("connect", () => {
-  socketRef.current.emit("joinRoom", userId);
-});
-
-
-  socketRef.current.on("newNotification", (notification) => {
-    setNotifications((prev) => [notification, ...prev]);
-  });
-
-  axios
-    .get(`https://asset-manager-new.onrender.com/api/notifications`, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-    .then((res) => setNotifications(res.data))
-    .catch(console.error);
-
-  return () => socketRef.current.disconnect();
-}, [userId]);
-
-
-  useEffect(() => {
-  console.log("Notifications updated:", notifications);
-}, [notifications]);
-
-
-  const markAllAsRead = async () => {
-    await axios.put(
-      `https://asset-manager-new.onrender.com/api/notifications/markAllRead/`,
-      {},
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-    setNotifications((prev) => prev.map((n) => ({ ...n, read: true })));
-  };
+  const { notifications, markAllAsRead } = useNotifications();
 
   return (
     <div className="notification-container">
@@ -71,8 +26,7 @@ useEffect(() => {
                   <h3>{n.title}</h3>
                   <p>{n.message}</p>
                   <div className="notification-footer">
-                   <span>{new Date(n.time || n.createdAt).toLocaleString()}</span>
-
+                    <span>{new Date(n.createdAt).toLocaleString()}</span>
                     {!n.isRead && <span className="badge">New</span>}
                   </div>
                 </div>
