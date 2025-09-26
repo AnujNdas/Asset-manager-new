f628aa6103e7bf40538bb16821d346271488f9aa
Anuj das <anujd339@gmail.com>
2025-09-26 15:57:14 +0530

    Update MisReport.jsx

--- frontend/src/Pages/MisReport.jsx
+++ frontend/src/Pages/MisReport.jsx
@@ -123,8 +123,8 @@ const MisReport = () => {
 
   return (
     <div className="mis-content">
-      <header>
         <h1 className="classify_heading">Asset Management Report</h1>
+      <header>
         <div className="navs">
           <button onClick={() => setActiveTab("hardware")}>Hardware</button>
           <button onClick={() => setActiveTab("software")}>Software</button>
