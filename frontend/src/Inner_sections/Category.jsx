df764622bb82b60d17efad8541d210e2667eccef
Anuj das <anujd339@gmail.com>
2025-10-08 15:51:25 +0530

    Update Category.jsx

--- frontend/src/Inner_sections/Category.jsx
+++ frontend/src/Inner_sections/Category.jsx
@@ -11,7 +11,7 @@ const Category = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  const perPage = 8; // works better with grid layout
+  const perPage = 9; // works better with grid layout
 
   const totalPages = Math.ceil(categories.length / perPage);
   const indexOfLast = currentPage * perPage;
