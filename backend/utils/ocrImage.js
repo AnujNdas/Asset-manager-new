f59e62f6a75e3192d9c4470abf760915dbef5153
Anuj das <anujd339@gmail.com>
2025-11-14 13:25:08 +0530

    Update ocrImage.js

--- backend/utils/ocrImage.js
+++ backend/utils/ocrImage.js
@@ -1,1 +1,18 @@
+// utils/ocrImage.js
+const Tesseract = require("tesseract.js");
+
+const ocrImage = async (filePath) => {
+  try {
+    const result = await Tesseract.recognize(filePath, "eng", {
+      logger: (m) => console.log("OCR Progress:", m),
+    });
+
+    return result.data.text;
+  } catch (err) {
+    console.error("Image OCR Error:", err);
+    return "";
+  }
+};
+
+module.exports = ocrImage;
 
