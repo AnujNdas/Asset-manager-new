1a95a13fbe8babb54e4c0d357ffd720605cceee3
Anuj das <anujd339@gmail.com>
2025-11-14 13:25:44 +0530

    Update extractTextFromFile.js

--- backend/utils/extractTextFromFile.js
+++ backend/utils/extractTextFromFile.js
@@ -1,1 +1,33 @@
+// utils/extractTextFromFile.js
+const ocrPdf = require("./ocrPdf");
+const ocrImage = require("./ocrImage");
+const fs = require("fs");
+
+const extractTextFromFile = async (file) => {
+  try {
+    const filePath = file.path;
+    const fileBuffer = fs.readFileSync(filePath);
+
+    const mime = file.mimetype;
+
+    let extractedText = "";
+
+    if (mime === "application/pdf") {
+      extractedText = await ocrPdf(fileBuffer, filePath);
+    } 
+    else if (mime.startsWith("image/")) {
+      extractedText = await ocrImage(filePath);
+    } else {
+      throw new Error("Unsupported file type: " + mime);
+    }
+
+    return extractedText;
+
+  } catch (err) {
+    console.error("OCR Engine Error:", err);
+    return "";
+  }
+};
+
+module.exports = extractTextFromFile;
 
