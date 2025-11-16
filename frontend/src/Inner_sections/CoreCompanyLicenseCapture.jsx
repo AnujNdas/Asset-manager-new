259ddc170830c4f1c9f8ea16bbeafd1dc0a4d430
Anuj das <anujd339@gmail.com>
2025-11-16 14:45:04 +0530

    Update CoreCompanyLicenseCapture.jsx

--- frontend/src/Inner_sections/CoreCompanyLicenseCapture.jsx
+++ frontend/src/Inner_sections/CoreCompanyLicenseCapture.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../page_styles/CoreLicenseCapture.css";
+import "../Page_styles/CoreLicenseCapture.css";
 
 const CoreCompanyLicenseCapture = () => {
   const [file, setFile] = useState(null);
