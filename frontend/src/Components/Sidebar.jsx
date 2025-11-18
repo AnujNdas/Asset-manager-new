b62ce2ec420257aa6415887c70d8b034098f7a2d
Anuj das <anujd339@gmail.com>
2025-11-18 16:02:26 +0530

    Update Sidebar.jsx

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,8 +1,7 @@
-import React, { useContext } from "react";
+import React from "react";
 import "../Component_styles/Sidebar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink } from "react-router-dom";
-import image from "../Images/logo.png";
 import {
   faGauge,
   faLayerGroup,
@@ -13,62 +12,105 @@ import {
   faRecycle,
   faExpand,
 } from "@fortawesome/free-solid-svg-icons";
-// import Switch from "./Switch";
 
 const Sidebar = ({ closeSidebar }) => {
-
   return (
-    <div className="sidebar-container">
-      <div className="sidebar">
-        <div className="sidebar-heading">
-          <div className="title">Vault<span>ifly</span><FontAwesomeIcon icon={faPaperPlane} style={{ marginLeft: "8px", color: "#6596ffff" }} /></div>
+    <div className="sidebar-container new-saas">
+      <div className="sidebar new-sidebar">
+        
+        {/* Logo / Branding */}
+        <div className="sidebar-heading new-heading">
+          <div className="title new-title">
+            Vault<span>ifly</span>
+            <FontAwesomeIcon
+              icon={faPaperPlane}
+              className="brand-icon"
+            />
+          </div>
         </div>
-        <div className="sidebar-menu">
+
+        {/* Menu */}
+        <div className="sidebar-menu new-menu">
           <ul>
             <li>
-              <NavLink to="/" onClick={closeSidebar} className={({ isActive }) => isActive ? "active" : ""}>
-                <FontAwesomeIcon icon={faGauge} style={{ width: "25%" }} />
+              <NavLink 
+                to="/" 
+                onClick={closeSidebar} 
+                className={({ isActive }) => isActive ? "active new-active" : ""}
+              >
+                <FontAwesomeIcon icon={faGauge} className="menu-icon"/>
                 <span className="tab-text">Dashboard</span>
               </NavLink>
             </li>
+
             <li>
-              <NavLink to="/assetCapture" onClick={closeSidebar} className={({ isActive }) => isActive ? "active" : ""}>
-                <FontAwesomeIcon icon={faCartShopping} style={{ width: "25%" }} />
+              <NavLink 
+                to="/assetCapture"
+                onClick={closeSidebar}
+                className={({ isActive }) => isActive ? "active new-active" : ""}
+              >
+                <FontAwesomeIcon icon={faCartShopping} className="menu-icon"/>
                 <span className="tab-text">Asset Capture</span>
               </NavLink>
             </li>
+
             <li>
-              <NavLink to="/inventory" onClick={closeSidebar} className={({ isActive }) => isActive ? "active" : ""}>
-                <FontAwesomeIcon icon={faLayerGroup} style={{ width: "25%" }} />
+              <NavLink 
+                to="/inventory"
+                onClick={closeSidebar} 
+                className={({ isActive }) => isActive ? "active new-active" : ""}
+              >
+                <FontAwesomeIcon icon={faLayerGroup} className="menu-icon"/>
                 <span className="tab-text">Inventory</span>
               </NavLink>
             </li>
+
             <li>
-              <NavLink to="/misReport" onClick={closeSidebar} className={({ isActive }) => isActive ? "active" : ""}>
-                <FontAwesomeIcon icon={faReceipt} style={{ width: "25%" }} />
+              <NavLink 
+                to="/misReport"
+                onClick={closeSidebar}
+                className={({ isActive }) => isActive ? "active new-active" : ""}
+              >
+                <FontAwesomeIcon icon={faReceipt} className="menu-icon"/>
                 <span className="tab-text">MIS Report</span>
               </NavLink>
             </li>
+
             <li>
-              <NavLink to="/classification" onClick={closeSidebar} className={({ isActive }) => isActive ? "active" : ""}>
-                <FontAwesomeIcon icon={faRecycle} style={{ width: "25%" }} />
+              <NavLink 
+                to="/classification"
+                onClick={closeSidebar}
+                className={({ isActive }) => isActive ? "active new-active" : ""}
+              >
+                <FontAwesomeIcon icon={faRecycle} className="menu-icon"/>
                 <span className="tab-text">Classifications</span>
               </NavLink>
             </li>
+
             <li>
-              <NavLink to="/scanner" onClick={closeSidebar} className={({ isActive }) => isActive ? "active" : ""}>
-                <FontAwesomeIcon icon={faExpand} style={{ width: "25%" }} />
+              <NavLink 
+                to="/scanner" 
+                onClick={closeSidebar}
+                className={({ isActive }) => isActive ? "active new-active" : ""}
+              >
+                <FontAwesomeIcon icon={faExpand} className="menu-icon"/>
                 <span className="tab-text">Scanner</span>
               </NavLink>
             </li>
+
             <li>
-              <NavLink to="/setting" onClick={closeSidebar} className={({ isActive }) => isActive ? "active" : ""}>
-                <FontAwesomeIcon icon={faGear} style={{ width: "25%" }} />
+              <NavLink 
+                to="/setting" 
+                onClick={closeSidebar}
+                className={({ isActive }) => isActive ? "active new-active" : ""}
+              >
+                <FontAwesomeIcon icon={faGear} className="menu-icon"/>
                 <span className="tab-text">Settings</span>
               </NavLink>
             </li>
           </ul>
         </div>
+
       </div>
     </div>
   );
