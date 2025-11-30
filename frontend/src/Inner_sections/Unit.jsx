5f8e4b2f744a02d17aaac158950676e55fd8d2df
Anuj das <anujd339@gmail.com>
2025-11-30 15:18:14 +0530

    Update Unit.jsx

--- frontend/src/Inner_sections/Unit.jsx
+++ frontend/src/Inner_sections/Unit.jsx
@@ -16,7 +16,7 @@ const Unit = () => {
 
   // Pagination
   const [currentPage, setCurrentPage] = useState(1);
-  const perPage = 9;
+  const perPage = 18;
   const totalPages = Math.ceil(filteredUnits.length / perPage);
   const indexOfLast = currentPage * perPage;
   const indexOfFirst = indexOfLast - perPage;
