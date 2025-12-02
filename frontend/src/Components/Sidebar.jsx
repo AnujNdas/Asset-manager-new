6be348e30207cfb19ce2ea5f781d6ad0466f125b
Anuj das <anujd339@gmail.com>
2025-12-02 16:49:00 +0530

    Update Sidebar.jsx

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState, useEffect} from "react";
 import "../Component_styles/Sidebar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink } from "react-router-dom";
-import image from "../Images/logo.png";
+import image from "../Images/Logo.png";
 import {
   faGauge,
   faLayerGroup,
