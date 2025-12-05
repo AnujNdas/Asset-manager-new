5dc0b2c0eb0d0f7eb70660b723a8a2f5d15b57ab
Anuj das <anujd339@gmail.com>
2025-12-05 20:20:31 +0530

    Update categoryRoutes.js

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -9,7 +9,10 @@ router.post('/', createCategory);
 // Route to get all categories
 router.get('/',getCategories);
 
-router.delete("/:id", deleteCategory);
+router.delete("/:id", (req, res, next) => {
+  console.log("Delete API hit for ID:", req.params.id);
+  next();
+}, deleteCategory);
 
 module.exports = router;
 
