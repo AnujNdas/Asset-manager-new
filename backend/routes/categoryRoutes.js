a7bdf29f97a8cabb88a3cd6df11ddd2f6f3c51cf
Anuj das <anujd339@gmail.com>
2025-12-08 13:05:05 +0530

    Update categoryRoutes.js

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,6 +1,6 @@
 // routes/categoryRoutes.js
 const express = require('express');
-const { createCategory , getCategories , deleteCategory }= require('../controllers/categoryControllers');
+const { createCategory , getCategories , deleteCategory , updateCategory }= require('../controllers/categoryControllers');
 const router = express.Router();
 
 // Route to create a category
@@ -9,6 +9,10 @@ router.post('/', createCategory);
 // Route to get all categories
 router.get('/',getCategories);
 
+// Route to update category 
+router.put('/update' , updateCategory)
+
+// Route to delete category 
 router.delete("/:id", (req, res, next) => {
   console.log("Delete API hit for ID:", req.params.id);
   next();
