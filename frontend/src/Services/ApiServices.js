8a6601fbd17fa295d19c02ef497cd366d64a6342
Anuj das <anujd339@gmail.com>
2025-12-08 16:28:27 +0530

    Update ApiServices.js

--- frontend/src/Services/ApiServices.js
+++ frontend/src/Services/ApiServices.js
@@ -219,7 +219,7 @@ export const getRecentAssets = async () =>
 export const getActiveUsers = async () =>
   axiosInstance.get("/admin/active-users").then((res) => res.data);
 
-export const getValuationTrend  = async () => {
+export const getMonthlyValuation  = async () => {
   const response = await axiosInstance.get("admin/valuation-trend");
   return response.data;
 };
