c25aabb02720d710b561ea159c31b127e70756a1
Anuj das <anujd339@gmail.com>
2025-12-08 15:20:07 +0530

    Update Footer.jsx

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -4,16 +4,18 @@ import "../Component_styles/Footer.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="copyright-box">
-        <span>Copyright © 2025{" "}</span>
-        <a
-          href="https://socialflylive.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Socialfly - Business Growth & Management System
-        </a>
-      </p>
+      <div className="marquee">
+        <div className="marquee-content">
+          Copyright © 2025{" "}
+          <a
+            href="https://socialflylive.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Socialfly - Business Growth & Management System
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
