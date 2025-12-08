e96e8a6dc314d19a7f300cfdce8dc99ab2c550e9
Anuj das <anujd339@gmail.com>
2025-12-08 13:08:09 +0530

    Update locationRoutes.js

--- backend/routes/locationRoutes.js
+++ backend/routes/locationRoutes.js
@@ -1,6 +1,6 @@
 // routes/locationRoutes.js
 const express = require('express');
-const { createLocation , getLocations , deleteLocation }= require('../controllers/locationControllers');
+const { createLocation , getLocations , deleteLocation , updateLocation }= require('../controllers/locationControllers');
 const router = express.Router();
 
 // Route to create a location
@@ -9,6 +9,8 @@ router.post('/', createLocation);
 // Route to get all locations
 router.get('/', getLocations);
 
+//Route to update locations 
+router.put('/update' , updateLocation )
 // Route to delete locations
 router.delete('/:id', deleteLocation);
 
