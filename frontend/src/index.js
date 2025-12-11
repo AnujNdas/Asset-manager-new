e589a938f30cfa4e514bedc88caf424ee0609039
Anuj das <155058838+AnujNdas@users.noreply.github.com>
2025-12-11 14:01:32 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './Context/ThemeContext';
 import { NotificationProvider } from './Context/NotificationContext';
-
+import { ProgressProvider } from "./Components/ProgressBar";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-      <ThemeProvider>
+      <ProgressProvider>
         <NotificationProvider>
           <App />
         </NotificationProvider>
-      </ThemeProvider>
+       </ProgressProvider> 
     </React.StrictMode>
   </BrowserRouter>
 );
