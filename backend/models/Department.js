479f97d9ce21c5235d158ba719c1b817a3ae412e
Anuj das <155058838+AnujNdas@users.noreply.github.com>
2025-12-15 14:25:35 +0530

    Create Department.js

---
+++ backend/models/Department.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const departmentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true
+    }
+  },
+  { timestamps: true }
+);
+
+// Normalize before validation
+departmentSchema.pre("validate", function (next) {
+  if (this.name) {
+    this.name =
+      this.name.charAt(0).toUpperCase() +
+      this.name.slice(1).toLowerCase();
+  }
+  next();
+});
+
+module.exports = mongoose.model("Department", departmentSchema);
