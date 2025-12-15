362a57b36d027f1a908176b249e320016298c0c0
Anuj das <155058838+AnujNdas@users.noreply.github.com>
2025-12-15 17:40:15 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,6 +17,7 @@ import AssetScanner from "./Pages/Scanner";
 import ForgotPassword from "./Inner_sections/ForgetPass";
 import ResetPassword from "./Inner_sections/ResetPass";
 import Footer from "./Components/Footer";
+import AssignmentPage from "./Components/AssignmentPage" ;
 
 const App = () => {
   const [profileUser, setProfileUser] = useState(null);
@@ -65,6 +66,7 @@ const App = () => {
         <Route path="/classification/*" element={<ProtectedRoute allowedRoles={["admin", "super-admin"]}><Classification /></ProtectedRoute>} />
         <Route path="/misreport" element={<ProtectedRoute allowedRoles={["admin" , "user", "super-admin"]}><MisReport /></ProtectedRoute>} />
         <Route path="/scanner" element={<ProtectedRoute allowedRoles={["admin", "super-admin"]}><AssetScanner /></ProtectedRoute>} />
+        <Route path="/assignment" element={<ProtectedRoute allowedRoles={["admin", "super-admin"]}><AssignmentPage /></ProtectedRoute>} />
       </Routes>
     </section>
     <section className={`fourth-container ${isUserPage ? 'blurred' : ''}`}>
