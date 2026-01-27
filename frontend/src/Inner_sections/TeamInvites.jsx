03403f2f9de283bbee8532439b90857ab3e9aff8
Anuj das <anujd339@gmail.com>
2026-01-27 12:49:15 +0530

    getting server code match the github repo

---
+++ frontend/src/Inner_sections/TeamInvites.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import InviteForm from "../Components/InviteForm";
+import InviteTable from "../Components/InviteTable";
+import { getInvites } from "../Services/ApiServices";
+import "../Page_styles/TeamInvites.css";
+const TeamInvites = () => {
+  const [invites, setInvites] = useState([]);
+
+  const loadInvites = async () => {
+    const res = await getInvites();
+    setInvites(res.data);
+  };
+
+  useEffect(() => {
+    loadInvites();
+  }, []);
+
+  return (
+    <div className="settings-container">
+      <h2>Team Invitations</h2>
+
+      <InviteForm onCreated={loadInvites} />
+      <InviteTable invites={invites} onRevoke={loadInvites} />
+    </div>
+  );
+};
+
+export default TeamInvites;
