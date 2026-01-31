55f119b9339cd85918d311baf4952a48e9533aa7
Anuj das <anujd339@gmail.com>
2026-01-31 22:45:49 +0530

    development under progress

--- frontend/src/Inner_sections/Help-Support.jsx
+++ frontend/src/Inner_sections/Help-Support.jsx
@@ -261,7 +261,7 @@ const loadMyTickets = async () => {
       <input
         type="text"
         name="name"
-        placeholder="Your Name"
+        placeholder="Name"
         value={contactData.name}
         onChange={handleContactChange}
       />
@@ -269,14 +269,14 @@ const loadMyTickets = async () => {
       <input
         type="email"
         name="email"
-        placeholder="Your Email"
+        placeholder="Email"
         value={contactData.email}
         onChange={handleContactChange}
       />
 
       <textarea
         name="message"
-        placeholder="Your Message"
+        placeholder="Message"
         value={contactData.message}
         onChange={handleContactChange}
       />
