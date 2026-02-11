8a00bfc6611c94a5e9c662d9c39bc5ddba91090d
Anuj das <anujd339@gmail.com>
2026-02-11 22:48:22 +0530

    ui changes

--- frontend/src/routes/SuperAdminRoutes.jsx
+++ frontend/src/routes/SuperAdminRoutes.jsx
@@ -1,6 +1,6 @@
   import { Route } from "react-router-dom";
   import ProtectedRoute from "../Components/ProtectedRoute";
-  import SuperAdminLayout from "../layouts/SuperAdminLayout";
+  import SuperAdminLayout from "../layout/SuperAdminLayout";
   import Analytics from "../Pages/super-admin/Analytics";
   import Dashboard from "../Pages/super-admin/SuperAdminDashboard";
   import Tenants from "../Pages/super-admin/Tenant";
