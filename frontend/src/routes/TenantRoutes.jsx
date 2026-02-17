91f1e89a04030992021bd04dda6e2f5fb1286e34
Anuj das <anujd339@gmail.com>
2026-02-17 21:43:32 +0530

    rebuild

--- frontend/src/routes/TenantRoutes.jsx
+++ frontend/src/routes/TenantRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route } from "react-router-dom";
-import TenantLayout from "../layouts/TenantLayout";
+import TenantLayout from "../layout/TenantLayout";
 import ProtectedRoute from "../Components/ProtectedRoute";
 
 import Dashboard from "../Pages/AdminDashboard";
@@ -8,9 +8,9 @@ import Inventory from "../Pages/Inventory";
 import Setting from "../Pages/Setting";
 import Classification from "../Pages/Classification";
 import MisReport from "../Pages/MisReport";
+import AssetScanner from "../Pages/Scanner";
 import AssignmentPage from "../Pages/AssignmentPage";
 import Onboarding from "../Pages/OnBoarding";
-// import Checkout from "../Pages/Checkout";
 import EmployeePage from "../Pages/Employee";
 const TenantRoutes = ({ profileUser }) => (
   <Route element={<ProtectedRoute allowedRoles={["admin", "user"]} />}>
@@ -21,10 +21,9 @@ const TenantRoutes = ({ profileUser }) => (
       <Route path="/setting/*" element={<Setting />} />
       <Route path="/classification/*" element={<Classification />} />
       <Route path="/misreport" element={<MisReport />} />
-      {/* <Route path="/scanner" element={<AssetScanner />} /> */}
+      <Route path="/scanner" element={<AssetScanner />} />
       <Route path="/assignment" element={<AssignmentPage />} />
       <Route path="/onboarding" element={<Onboarding />} />
-      {/* <Route path="/checkout" element={<Checkout />} /> */}
       <Route path="/employee" element={<EmployeePage />} />
     </Route>
   </Route>
