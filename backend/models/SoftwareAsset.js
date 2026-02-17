a35e0591eae8e6ac44b2a70b396569c9fba7c548
Anuj das <anujd339@gmail.com>
2026-02-17 23:12:44 +0530

    design

--- backend/models/SoftwareAsset.js
+++ backend/models/SoftwareAsset.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const costSchema = require("./Costschema");
+const costSchema = require("./CostSchema");
 
 const SoftwareAssetSchema = new mongoose.Schema(
   {
