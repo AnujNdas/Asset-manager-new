a185d8a32c2643524d2dff8ad3cc70e9c996f2e6
Anuj das <anujd339@gmail.com>
2026-03-01 12:34:01 +0530

    logging the response

--- backend/utils/getRazorPayPlanId.js
+++ backend/utils/getRazorPayPlanId.js
@@ -7,6 +7,7 @@ const getRazorpayPlanId = (tier, billingCycle) => {
 
   const mode =
     process.env.NODE_ENV === "production" ? "live" : "test";
+    console.log(`Using Razorpay ${mode} plan for ${tier} ${billingCycle}`);
 
   const planId = plans[tier][billingCycle][mode];
 
