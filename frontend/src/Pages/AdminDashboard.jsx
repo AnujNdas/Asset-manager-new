a7f4372d8baf782e0c712b771920ce96fa02ab7f
Anuj das <anujd339@gmail.com>
2026-03-03 23:56:49 +0530

    fixing dashboard data

--- frontend/src/Pages/AdminDashboard.jsx
+++ frontend/src/Pages/AdminDashboard.jsx
@@ -594,7 +594,7 @@ const SpendByCategoryDonutChart = ({ data }) => {
 
   return (
     <div className="donut-wrapper">
-      <ResponsiveContainer width="90%" height="90%">
+      <ResponsiveContainer width="100%" height="100%">
         <PieChart>
           <Pie
             data={data}
