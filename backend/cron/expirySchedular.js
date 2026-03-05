01e650f847126554a56e6f448b1bdf172d8abbd6
Anuj das <anujd339@gmail.com>
2026-03-05 21:05:58 +0530

    cron job check

--- backend/cron/expirySchedular.js
+++ backend/cron/expirySchedular.js
@@ -1,6 +1,7 @@
 const cron = require("node-cron");
 const sendExpiryAlerts = require("../jobs/sendExpiryAlerts");
-
+// run once immediately (for testing)
+sendExpiryAlerts();
 cron.schedule("0 9 * * *", async () => {
   console.log("Running expiry alert cron...");
   await sendExpiryAlerts();
