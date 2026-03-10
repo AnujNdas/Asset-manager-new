ff5eda5991918ac309bbb4590a43acabe3779873
Anuj das <anujd339@gmail.com>
2026-03-10 16:13:30 +0530

    limits set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
-import { Routes, Route } from "react-router-dom";
-import { useEffect, useState, lazy, Suspense } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import TenantRoutes from "./routes/TenantRoutes";
+import SuperAdminRoutes from "./routes/SuperAdminRoutes";
+
+import User from "./Pages/User";
+import Login from "./Inner_sections/Login";
+import Signup from "./Inner_sections/Signup";
+import ForgotPassword from "./Inner_sections/ForgetPass";
+import ResetPassword from "./Inner_sections/ResetPass";
 import { Toaster } from "react-hot-toast";
-
-const TenantRoutes = lazy(() => import("./routes/TenantRoutes"));
-const SuperAdminRoutes = lazy(() => import("./routes/SuperAdminRoutes"));
-
-const User = lazy(() => import("./Pages/User"));
-const Login = lazy(() => import("./Inner_sections/Login"));
-const Signup = lazy(() => import("./Inner_sections/Signup"));
-const ForgotPassword = lazy(() => import("./Inner_sections/ForgetPass"));
-const ResetPassword = lazy(() => import("./Inner_sections/ResetPass"));
-const Maintenance = lazy(() => import("./Pages/Maintainence"));
-const Unauthorized = lazy(() => import("./Pages/Unauthorized"));
-
+import Maintenance from "./Pages/Maintainence";
+import Unauthorized from "./Pages/Unauthorized";
+import "./App.css";
 const App = () => {
   const [profileUser, setProfileUser] = useState(null);
 
@@ -20,35 +19,26 @@ const App = () => {
     const storedUsername = localStorage.getItem("username");
     if (storedUsername) setProfileUser(storedUsername);
   }, []);
-
+  
   if (process.env.REACT_APP_MAINTENANCE_MODE === "true") {
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Maintenance />
-      </Suspense>
-    );
-  }
+  return <Maintenance />;
+}
 
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/user" element={<User />}>
-            <Route
-              path="login"
-              element={<Login setProfileUser={setProfileUser} />}
-            />
-            <Route path="signup" element={<Signup />} />
-            <Route path="forgot" element={<ForgotPassword />} />
-            <Route path="reset/:token" element={<ResetPassword />} />
-          </Route>
-
-          <Route path="/unauthorized" element={<Unauthorized />} />
-
-          <TenantRoutes profileUser={profileUser} />
-          <SuperAdminRoutes />
-        </Routes>
-      </Suspense>
+      <Routes>
+        {/* Auth / User routes */}
+        <Route path="/user" element={<User />}>
+          <Route path="login" element={<Login setProfileUser={setProfileUser} />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="forgot" element={<ForgotPassword />} />
+          <Route path="reset/:token" element={<ResetPassword />} />
+          
+        </Route>
+        <Route path="/unauthorized" element={<Unauthorized />} />
+        {TenantRoutes({ profileUser })}
+        {SuperAdminRoutes()}
+      </Routes>
 
       <Toaster position="top-right" />
     </>
