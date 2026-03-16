2333011e0d4dd0d3fe0c10c22570bb8ae39d3e46
Anuj das <anujd339@gmail.com>
2026-03-16 17:09:51 +0530

    ui fixes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -32,13 +32,13 @@ root.render(
 // ---------------------------
 // 📌 Register Service Worker
 // ---------------------------
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register('/service-worker.js')
-      .then((reg) => console.log('Service Worker registered:', reg))
-      .catch((err) => console.log('Service Worker registration failed:', err));
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register('/service-worker.js')
+//       .then((reg) => console.log('Service Worker registered:', reg))
+//       .catch((err) => console.log('Service Worker registration failed:', err));
+//   });
+// }
 
 reportWebVitals();
