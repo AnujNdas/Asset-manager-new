47e44b5da8a4ce22bfedaaea9fe5d95856f3548d
Anuj das <anujd339@gmail.com>
2026-04-17 14:28:49 +0530

    Stop tracking backend .env

--- backend/routes/publicRoutes.js
+++ backend/routes/publicRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { getPublicTrackedInstance } = require("../controllers/publicControllers");
-router.get("/tracking/:id", getPublicTrackedInstance);
+router.get("/:id", getPublicTrackedInstance);
 
 module.exports = router;
