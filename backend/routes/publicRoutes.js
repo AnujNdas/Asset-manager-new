32a7c83b75c27ae059f85cb018c9ad4443fd700d
Anuj das <anujd339@gmail.com>
2026-04-27 14:20:13 +0530

    re constructing inventory

--- backend/routes/publicRoutes.js
+++ backend/routes/publicRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authenticateToken = require("../Middleware/authenticateToken");
+const authenticateToken = require("../Middleware/Authentication-token");
 const tenantMiddleware = require("../Middleware/tenantMiddleware");
 const requireActiveSubscription = require("../Middleware/requireActiveSubscription");
 const { getTrackedInstance } = require("../controllers/publicControllers");
