933cddd9de3d320ad87004cc6b668a62947271ce
Anuj das <anujd339@gmail.com>
2026-05-19 22:02:16 +0530

    re constructing inventory

--- backend/config/razorpayPlans.js
+++ backend/config/razorpayPlans.js
@@ -2,7 +2,7 @@ const plans = {
   base: {
     monthly: {
       test: "plan_SKhqqkAt4aPUqs",
-      live: "plan_SOJbnSVHVD2Jhw",
+      live: "plan_SKhXz6JtWNagJG",
       price: 15,
     },
     yearly: {
