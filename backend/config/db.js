8ac68c84dfd60d2883b7df4d85231aacead1be03
vaultifly-ops <vaultifly@gmail.com>
2026-06-15 01:15:59 +0530

    origin access setup

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,10 @@ const connectDB = async () => {
         await mongoose.connect(process.env.MONGO_URI); // No need to pass options here
 
         console.log("Connected successfully to MongoDB");
+        console.log(
+            "Database:",
+            mongoose.connection.db.databaseName
+          );
     } catch (error) {
         console.error("MongoDB connection failed:", error.message);
         process.exit(1);
