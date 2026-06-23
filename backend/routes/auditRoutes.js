288bd973e32a6046e3002469de21b4cbb7f59ed4
AnujNdas <anujd339@gmail.com>
2026-06-23 15:59:49 -0700

    mismatched fixed

--- backend/routes/auditRoutes.js
+++ backend/routes/auditRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authenticationToken = require("../Middleware/Authentication-token");
+const authenticateToken = require("../Middleware/Authentication-token");
 const {
   getAuditDashboard,
     getFinancialAudit,
