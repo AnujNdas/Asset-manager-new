82835c643fc251e3f46c712236a815b3cfbfaefc
AnujNdas <anujd339@gmail.com>
2026-09-06 01:02:10 -0700

    blog fixed and added

--- frontend/src/Pages/LandingPage/About.jsx
+++ frontend/src/Pages/LandingPage/About.jsx
@@ -7,10 +7,10 @@ import {
   FaLinkedinIn,
   FaPinterestP,
   FaTwitter,
-  FaThreads,
   FaWhatsapp,
   FaTelegramPlane,
 } from "react-icons/fa";
+import { FaThreads } from "react-icons/fa6";
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 const values = [
